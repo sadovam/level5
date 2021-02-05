c32ca53a1bd0f8abc5f02e1cd56d426f151c1166
Andrii Sadov <sadovam@gmail.com>
2021-02-05 17:05:59 +0200

    add babel

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,24 @@
-!function n(o,u,i){function f(r,e){if(!u[r]){if(!o[r]){var t="function"==typeof require&&require;if(!e&&t)return t(r,!0);if(l)return l(r,!0);throw(t=new Error("Cannot find module '"+r+"'")).code="MODULE_NOT_FOUND",t}t=u[r]={exports:{}},o[r][0].call(t.exports,function(e){return f(o[r][1][e]||e)},t,t.exports,n,o,u,i)}return u[r].exports}for(var l="function"==typeof require&&require,e=0;e<i.length;e++)f(i[e]);return f}({1:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.sayHello=void 0,t.sayHello=function(e){return"Hello from "+e}},{}],2:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n,o,u=e("./greet");n="greeting",o="Type Script",document.getElementById(n).innerText=u.sayHello(o)},{"./greet":1}]},{},[2]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayHello = void 0;
+function sayHello(name) {
+    return "Hello from " + name;
+}
+exports.sayHello = sayHello;
+
+},{}],2:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var greet_1 = require("./greet");
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "Type Script");
+
+},{"./greet":1}]},{},[2])
+
 //# sourceMappingURL=bundle.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var tsify = require("tsify");
-var uglify = require("gulp-uglify");
 var sourcemaps = require("gulp-sourcemaps");
 var buffer = require("vinyl-buffer");
 var paths = {
@@ -24,11 +23,14 @@ gulp.task(
       packageCache: {},
     })
       .plugin(tsify)
+      .transform("babelify", {
+        presets: ["es2015"],
+        extensions: [".ts"],
+      })
       .bundle()
       .pipe(source("bundle.js"))
       .pipe(buffer())
       .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(uglify())
       .pipe(sourcemaps.write("./"))
       .pipe(gulp.dest("dist"));
   })

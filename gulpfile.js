66b71d34c2b959cf704e3bd695b8d42ed2f0e61f
Andrii Sadov <sadovam@gmail.com>
2021-02-11 19:36:28 +0200

    Add linter

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var tsify = require("tsify");
 var sourcemaps = require("gulp-sourcemaps");
 var buffer = require("vinyl-buffer");
 var paths = {
-  pages: ["src/*.html"],
+  pages: ["src/*.html", "src/*.css"],
 };
 
 gulp.task("copy-html", function () {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
-import { sayHello } from "./greet";
+import * as moment from 'moment';
 
-function showHello(divName: string, name: string) {
-  const elt = document.getElementById(divName);
-  elt.innerText = sayHello(name);
+const timeLabel = document.getElementById('timer__time');
+
+function toggleButtons() {
+  timeLabel.innerHTML = '0';
+  const elms = document.getElementsByClassName('btn');
+  for (let i = 0; i < elms.length; i += 1) {
+    elms[i].classList.toggle('hidden');
+  }
 }
 
-showHello("greeting", "Type Script");
+function tick(time: number) {
+  timeLabel.innerHTML = moment.unix(time).format('mm:ss');
+  if (time === 0) {
+    toggleButtons();
+    return;
+  }
+  setTimeout(() => tick(time - 1), 1000);
+}
+
+function incTime() {
+  const timeDuration = Number(timeLabel.innerHTML) + 1;
+  timeLabel.innerHTML = String(timeDuration);
+}
+
+function decTime() {
+  let timeDuration = Number(timeLabel.innerHTML);
+  if (timeDuration > 0) timeDuration -= 1;
+  timeLabel.innerHTML = String(timeDuration);
+}
+
+function start() {
+  const timeDuration = Number(timeLabel.innerHTML) * 60;
+  if (timeDuration === 0) return;
+  toggleButtons();
+  tick(timeDuration);
+}
+
+const incBtn = document.getElementById('timer__inc-btn');
+incBtn.onclick = incTime;
+const decBtn = document.getElementById('timer__dec-btn');
+decBtn.onclick = decTime;
+const startBtn = document.getElementById('timer__start-btn');
+startBtn.onclick = start;
